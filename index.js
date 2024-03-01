7a61467358f0a00f6677d24be72a94f09c7d874d
RithikaSundaram <rithika.s2022cce@sece.ac.in>
2024-03-01 10:48:44 +0530

    express commit

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+//this should be outside the static files since static files should contain only the 
+//frontend files
+//Express --> Live server
+//when we use live server if we update the code it will automatically updates in the browser
+//but with express if we want to use that -->nodemon (npm package)
+//nodemon-->automatically updates
+
+// http://127.0.0.1.3000/hi?phone=12
+//while passing the parameters give it with ?
+
 const express = require('express')
 const app = express()
 
 const static = express.static("static")
-app.use("/",static)
+app.use("/",static)  // if we use /forms instead of / --> cannot GET/
+// we can serve ststic files in any path here we are using /
+//app.use -- > app.get
 
-app.listen(8000, () =>{
+
+app.get("/hi", (req,res)=>{
+    console.log(req.query); 
+    //res.send("Hello");
+    res.json({
+        name: req.query.name,
+        description:req.query.description,
+        date:req.query.date,
+        
+    })
+   
+});
+
+app.listen(3000, () =>{
     console.log("App Running");
 });
