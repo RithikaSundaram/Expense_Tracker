93644572655664fd53349885083920a3bfcdd2bc
RithikaSundaram <rithika.s2022cce@sece.ac.in>
2024-03-01 14:45:37 +0530

    dynamic endpoints

--- index.js
+++ index.js
@@ -42,14 +42,31 @@ app.post("/hi", (req,res)=>{
 });
 
 //fetch is an package
-app.get("/todos", async(req,res) => {
+//todos is an third party api
+/*app.get("/todos", async(req,res) => {
     //fetch('https://jsonplaceholder.typicode.com/todos/1')
    //.then((response)=>response.json())
    //.then((json)=>res.json(json));
    const response = await fetch("https://jsonplaceholder.typicode.com/todos")
    const todos = await response.json()
    res.json(todos)
+});*/
+
+app.get("/todos/2", async(req,res) => {
+    //fetch('https://jsonplaceholder.typicode.com/todos/1')
+   //.then((response)=>response.json())
+   //.then((json)=>res.json(json));
+   const response = await fetch("https://jsonplaceholder.typicode.com/todos/2")
+   const todos = await response.json()
+   res.json(todos)
 });
+
+//to generate the todos for many times
+//id-->key(here)
+app.get("/todos/:id",async(req,res) => {
+    const {id:todoId} = req.params;
+    res.json(todoId)
+})
 
 app.listen(5001, () =>{
     console.log("App Running");
